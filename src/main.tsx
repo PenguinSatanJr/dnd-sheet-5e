b061aaf3a7343103c564604e9c01292459a72914
PenguinSatanJr <penguinsatan107@gmail.com>
2025-09-10 20:56:55 +0400

    initial setup of react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import {StrictMode} from "react";
 import {createRoot} from "react-dom/client";
 import App from "./App";
+import {BrowserRouter, Route, Routes} from "react-router";
 
 const rootElement = document.getElementById("root");
 
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
     </StrictMode>
   );
 } else {
